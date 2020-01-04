e35adc90f947d47bc3ebfd46e5bd5528b9478fbd
ProffOak <skrot1234@hotmail.com>
2020-01-04 18:53:17 +0100

    First login to spotify done

--- spotifyquiz/src/pages/home.js
+++ spotifyquiz/src/pages/home.js
@@ -1,13 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Grid from "@material-ui/core/Grid";
+import SpotifyLogin from "../components/spotify/SpotifyLogin";
+
+
+
 
 class home extends Component {
   componentDidMount() {}
   render() {
     return (
+        <Fragment>
       <div style={{ textAlign: "center" }}>
         <p>You are on the home page</p>
       </div>
+    <SpotifyLogin />
+        </Fragment>
     );
   }
 }
